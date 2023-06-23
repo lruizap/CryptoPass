0b00effbaf349ee697f78ef9a04723131bd8ac83
lruizap <lruizap2502@gmail.com>
2023-06-23 10:40:34 +0200

    first commit

---
+++ CryptoPass-App/script/passwordGenerator.py
@@ -0,0 +1,21 @@
+import string
+import secrets
+
+
+def passwordGenerator(length: int, symbols: bool, uppercase: bool):
+    combination = string.ascii_lowercase + string.digits
+
+    if symbols:
+        combination += string.punctuation
+
+    if uppercase:
+        combination += string.ascii_uppercase
+
+    combination_length = len(combination)
+
+    new_password = ""
+
+    for _ in range(length):
+        new_password += combination[secrets.randbelow(combination_length)]
+
+    return new_password
---
+++ README.md
@@ -0,0 +1,7 @@
+# CryptoPass
+
+Este proyecto se basa en una aplicación de escritorio que genere contraseñas seguras de forma automática.
+
+Las contraseñas se pueden comprobar en la siguiente web:
+
+[security.org](https://www.security.org/how-secure-is-my-password/)
